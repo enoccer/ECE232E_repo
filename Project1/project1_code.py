f70c2e3edb74af827f410dfcb7cebaa9b0531c00
Enoch Yeo <eyeo@Mac.lan>
2026-07-04 02:42:02 -0700

    Finished draft of Part 1, Question 1 a-d.

--- Project1/project1_code.py
+++ Project1/project1_code.py
@@ -41,14 +41,186 @@ def question_1b():
 	ps = [0.002, 0.006, 0.012, 0.045, 0.1]
 	n = 900
 	trials = 20
+	summaries = {}
 	
 	for p in ps:
 		rows = [er_summary(n, p) for i in range(trials)]
 		prob_connected = np.mean([row["connected"] for row in rows])
 		avg_gcc_fraction = np.mean([row["gcc_fraction"] for row in rows])
-		print(f"p={p:0.3f} P(conn)~{prob_connected:0.2f} avg GCC frac~{avg_gcc_fraction:0.2f}")
+		avg_gcc_diam = np.mean([row["gcc_diameter"] for row in rows])
+		print(f"p={p:0.3f} P(conn)~{prob_connected:0.2f}, " + 
+			  f"avg GCC frac~{avg_gcc_fraction:0.2f}, " + 
+			  f"avg GCC diam~{avg_gcc_diam:0.2f}")
+		# Cache for outside loop
+		summaries[p] = rows
 	
+	# Find an instance of a disconnected ER network
+	found = False
+	for p, summary in summaries.items():
+		for run in range(trials):
+			if summary[run]["connected"] == False:
+				diam = summary[run]["gcc_diameter"]
+				print(f"GCC diameter={diam} for a disconnected network (p={p}) instance")
+				return
 	return
+	
+def threshold_condition_index(x_vals, y_vals, threshold):
+	"""Finds first index by which threshold condition is satisfied."""
+	
+	condition = y_vals > threshold
+	if np.any(condition):
+		# np.argmax() returns the index of the FIRST 'True' value
+		first_index = np.argmax(condition)
+		
+		x_val = x_vals[first_index]
+		y_val = y_vals[first_index]
+		
+		print(f"First X value above Y > {threshold}: {x_val:.5f}")
+		print(f"Actual Y value at that point: {y_val:.5f}")
+		return x_val, y_val
+	else:
+		print(f"The Y value never reaches the threshold of {threshold}")
+		return None, None
+
+def question_1c():
+	n = 900
+	ps = np.linspace(0, 0.005, 25)
+	n_networks = 100
+	gcc_sizes = []
+	for p in ps:
+		networks = [er_summary(n, p) for i in range(n_networks)]
+		avg_norm_gcc_size = np.mean([row["gcc_fraction"] for row in networks])
+		gcc_sizes.append(avg_norm_gcc_size)
+	
+	# Scatter
+	plt.scatter(ps, gcc_sizes, label='Data points')
+	
+	# Trend line
+	x_data, y_data = ps, gcc_sizes
+	coefficients = np.polyfit(x_data, y_data, deg=4)
+	poly_function = np.poly1d(coefficients)
+	x_smooth = np.linspace(x_data.min(), x_data.max(), 200)
+	y_fitted_curve = poly_function(x_smooth)
+	
+	plt.plot(x_smooth, y_fitted_curve, 
+			 color='limegreen', linestyle='--', linewidth=2, label='Trend line')
+	
+	plt.title('Normalized GCC size v. probability')
+	plt.xlabel('Probability p')
+	plt.ylabel('Normalized GCC size')
+	plt.grid(True, linestyle='--', alpha=0.5)
+	plt.legend()
+	plt.show()
+	
+	# For answering parts i and ii
+	gcc_vals = np.array(gcc_sizes)
+	p_i, gcc_i = threshold_condition_index(ps, gcc_vals, 0.005)
+	p_ii, gcc_ii = threshold_condition_index(ps, gcc_vals, 0.98)
+	print(f"\nPart (i) p = {p_i:.5f}, gcc_size = {gcc_i:.5f}")
+	print(f"Part (ii) p = {p_ii:.5f}, gcc_size = {gcc_ii:.5f}")
+	
+def simulate_across_node_sizes(nodes, c, n_networks):
+	"""
+	Randomly generates `n_networks` using the average node value `c`.
+	The random network generation occurs across a range of node sizes `nodes`.
+	"""
+	
+	gcc_sizes = []
+	for n in nodes:
+		p = c/n  # Edge-formation probability
+		networks = [er_summary(n, p) for i in range(n_networks)]
+		avg_norm_gcc_size = np.mean([row["gcc_fraction"] for row in networks])
+		gcc_sizes.append(avg_norm_gcc_size)
+		
+	return gcc_sizes
+	
+def question_1d(c):
+	"""c is the average degree of nodes."""
+	nodes = np.linspace(100, 1e4, 25).astype(int)
+	n_networks = 100
+	gcc_sizes = simulate_across_node_sizes(nodes, c, n_networks)
+
+	# Scatter
+	plt.scatter(nodes, gcc_sizes, label='Data points')
+	
+	plt.title(f"Normalized GCC size v. nodes (c={c})")
+	plt.xlabel('Number of nodes n')
+	plt.ylabel('Normalized GCC size')
+	plt.grid(True, linestyle='--', alpha=0.5)
+	plt.legend()
+	plt.show()
+	
+def log_fit(x_data, y_data, x_start, x_end):
+	"""Generates a logarithmic fit."""
+	
+	# Fit a line to: y = a * ln(x) + b
+	a, b = np.polyfit(np.log(x_data), y_data, 1)
+	
+	# Make a smooth curve line to plot
+	x_smooth = np.linspace(x_start, x_end, 200)
+	y_fitted = a * np.log(x_smooth) + b
+	
+	return x_smooth, y_fitted
+	
+def question_1d_logfit(c):
+	"""c is the average degree of nodes."""
+	n_start, n_end = 100, 1e4
+	nodes = np.linspace(n_start, n_end, 25).astype(int)
+	n_networks = 50
+	
+	# Random network simulations
+	gcc_sizes = simulate_across_node_sizes(nodes, c, n_networks)
+
+	# Scatter
+	plt.scatter(nodes, gcc_sizes, label='Simulation data')
+	
+	# Logarithm fit
+	x_data, y_data = nodes, gcc_sizes
+	x_smooth, y_fitted = log_fit(x_data, y_data, n_start, n_end)
+	
+	# Plot fit
+	plt.plot(x_smooth, y_fitted, color='navy', linestyle='--', linewidth=2.5,
+             label='Fitted Line')
+	
+	plt.title(f"Normalized GCC size v. nodes (c={c})")
+	plt.xlabel('Number of nodes n')
+	plt.ylabel('Normalized GCC size')
+	plt.grid(True, linestyle='--', alpha=0.5)
+	plt.legend()
+	plt.show()
+
+
+def question_1d_part_iv():
+	c_vals = [1.15, 1.25, 1.35]
+	n_start, n_end = 100, 1e4
+	nodes = np.linspace(n_start, n_end, 25).astype(int)
+	n_networks = 100
+	plt.figure(figsize=(8, 5))
+	
+	for c in c_vals:
+		print(f"Generating random networks for c={c}")
+		# Random network simulations
+		gcc_sizes = simulate_across_node_sizes(nodes, c, n_networks)
+		
+		# Logarithm fit
+		x_data, y_data = nodes, gcc_sizes
+		x_smooth, y_fitted = log_fit(x_data, y_data, n_start, n_end)
+
+		# Plot fit
+		plt.plot(x_smooth, y_fitted, linestyle='--', linewidth=2.5,
+				 label=f'Fitted Line (c={c})')
+
+	plt.title(f"Normalized GCC size v. nodes (c={c})")
+	plt.xlabel('Number of nodes n')
+	plt.ylabel('Normalized GCC size')
+	plt.grid(True, linestyle='--', alpha=0.5)
+	plt.legend()
+	plt.show()
+
+
+
+
+
 
 
 
